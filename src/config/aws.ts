b5eef260ddab24b603924662a2ea79f1df0dda3c
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-03-13 13:00:35 +0000

    UPDATE: create websocket connection in browser and update aws config type

--- src/config/aws.ts
+++ src/config/aws.ts
@@ -1,14 +1,17 @@
 import AWS from "aws-sdk";
 import dotenv from "dotenv";
+import { ServiceConfigurationOptions } from 'aws-sdk/lib/service';
 
 dotenv.config()
 
-AWS.config.update({
+const serviceConfigOptions : ServiceConfigurationOptions = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   sessionToken: process.env.AWS_SESSION_TOKEN,
   region: process.env.AWS_REGION,
   endpoint: process.env.AWS_ENDPOINT,
-});
+}
+
+AWS.config.update(serviceConfigOptions);
 
 export const dynamodb = new AWS.DynamoDB.DocumentClient()
